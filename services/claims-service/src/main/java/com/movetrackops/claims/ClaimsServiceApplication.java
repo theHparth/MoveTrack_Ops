78e934c486e6c7c379e6deeb492b61ea24f27efe
Parth <pphirpara@gmail.com>
2026-08-12 18:02:47 -0400

    feat: add claims approval threshold config

--- services/claims-service/src/main/java/com/movetrackops/claims/ClaimsServiceApplication.java
+++ services/claims-service/src/main/java/com/movetrackops/claims/ClaimsServiceApplication.java
@@ -2,8 +2,10 @@ package com.movetrackops.claims;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ClaimsServiceApplication {
 
 	public static void main(String[] args) {
