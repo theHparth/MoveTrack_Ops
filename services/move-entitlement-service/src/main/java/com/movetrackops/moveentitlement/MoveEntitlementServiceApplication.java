a858cafb7454d412a6c530e6961f3653a45f1261
Parth <pphirpara@gmail.com>
2026-08-12 10:52:07 -0400

    feat: scaffold move-entitlement-service with spring initializr

---
+++ services/move-entitlement-service/src/main/java/com/movetrackops/moveentitlement/MoveEntitlementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.movetrackops.moveentitlement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveEntitlementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveEntitlementServiceApplication.class, args);
+	}
+
+}
