656bc285162a933a54f6d3de09aa9abe99b5e063
Parth <pphirpara@gmail.com>
2026-08-13 17:32:12 -0400

    feat: add change control record entity and repository

---
+++ services/move-entitlement-service/src/main/java/com/movetrackops/moveentitlement/repository/ChangeControlRecordRepository.java
@@ -0,0 +1,7 @@
+package com.movetrackops.moveentitlement.repository;
+
+import com.movetrackops.moveentitlement.entity.ChangeControlRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChangeControlRecordRepository extends JpaRepository<ChangeControlRecord, Long> {
+}
