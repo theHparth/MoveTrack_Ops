b462b1a4aadf374bc89858da878662f34be4031f
Parth <pphirpara@gmail.com>
2026-08-15 23:00:20 -0400

    publish move.created to rabbitmq

--- services/move-entitlement-service/src/test/java/com/movetrackops/moveentitlement/service/EntitlementCalculationServiceTest.java
+++ services/move-entitlement-service/src/test/java/com/movetrackops/moveentitlement/service/EntitlementCalculationServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 import java.util.Optional;
 
@@ -26,11 +27,14 @@ class EntitlementCalculationServiceTest {
     @Mock
     private WeightAllowanceRepository weightAllowanceRepository;
 
+    @Mock
+    private RabbitTemplate rabbitTemplate;
+
     private EntitlementCalculationService calculationService;
 
     @BeforeEach
     void setUp() {
-        calculationService = new EntitlementCalculationService(moveRequestRepository, weightAllowanceRepository);
+        calculationService = new EntitlementCalculationService(moveRequestRepository, weightAllowanceRepository, rabbitTemplate);
     }
 
     @Test
